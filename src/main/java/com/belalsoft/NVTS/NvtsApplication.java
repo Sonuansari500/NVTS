8768e2536aad1e04e6fe524b7f579364afb77bb8
iqbal <sonuansari500@gmail.com>
2023-01-28 21:17:48 +0530

    initial setup is done

---
+++ src/main/java/com/belalsoft/NVTS/NvtsApplication.java
@@ -0,0 +1,13 @@
+package com.belalsoft.NVTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belalsoft/NVTS/NvtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belalsoft.NVTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
